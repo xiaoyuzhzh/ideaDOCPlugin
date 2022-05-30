71d407c41c36c501b550f3b198c486467726501a
GitHub Action <action@github.com>
2022-05-30 12:47:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/xiaoyuzhzh/ideadocplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xiaoyuzhzh/ideadocplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xiaoyuzhzh.ideadocplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xiaoyuzhzh.ideadocplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/xiaoyuzhzh/ideadocplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/xiaoyuzhzh/ideadocplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.xiaoyuzhzh.ideadocplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xiaoyuzhzh.ideadocplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
